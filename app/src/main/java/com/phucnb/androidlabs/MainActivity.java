0df118ef9725fb36126108e3bdfe5147740f59e3
Phuc Nguyen <kufuc2604@gmail.com>
2019-01-21 14:59:35 -0500

    Grid, Linear and relative layout for lab2

--- app/src/main/java/com/phucnb/androidlabs/MainActivity.java
+++ app/src/main/java/com/phucnb/androidlabs/MainActivity.java
@@ -1,5 +1,7 @@
 package com.phucnb.androidlabs;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_grid);
     }
+
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        SharedPreferences emailPref = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.M
+//    }
 }
