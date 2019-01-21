12aafabf567e91359b05ae0474689d224030d074
Phuc Nguyen <kufuc2604@gmail.com>
2019-01-21 15:56:25 -0500

    Lab3 - SHaredPreferences

--- app/src/main/java/com/phucnb/androidlabs/MainActivity.java
+++ app/src/main/java/com/phucnb/androidlabs/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String SHARED_PREFERENCES_NAME = "emailPref";
+    private Button loginBtn;
+    private EditText email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_lab3);
+
+        email = (EditText) findViewById(R.id.editText2);
+
+        loginBtn = (Button) findViewById(R.id.LoginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences emailPref = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor edit = emailPref.edit();
+               // edit.putString(email.getText());
+                edit.commit();
+            }
+        });
     }
 
 //    @Override
